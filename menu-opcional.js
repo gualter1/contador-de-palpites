e4eb5c81548e7f00e89a4ca88af93b8befc19d95
gualter1 <adison_gualter@outlook.com>
2025-01-03 10:41:07 -0300

    alteraçoes no rodape,  coreeçoes no cabeçalho, criaçao de politica de privacidade

---
+++ menu-opcional.js
@@ -0,0 +1,20 @@
+{/* <script>
+    document.addEventListener("DOMContentLoaded", function () {
+        const dropdownToggle = document.querySelector(".dropdown-toggle");
+        const dropdownContent = document.querySelector(".dropdown-content");
+
+        dropdownToggle.addEventListener("click", function () {
+            dropdownContent.style.display = 
+                dropdownContent.style.display === "block" ? "none" : "block";
+        });
+
+        // Fecha o menu se clicar fora
+        document.addEventListener("click", function (event) {
+            if (!dropdownToggle.contains(event.target) && !dropdownContent.contains(event.target)) {
+                dropdownContent.style.display = "none";
+            }
+        });
+    });
+</script> */}
+
+//talvez vai precisar implementar esta opçao
